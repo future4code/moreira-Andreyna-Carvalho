30e3a3260b18d67916a5d6ef379f3fd9227ec1ba
Andreyna Carvalho <87716793+andreyna1808@users.noreply.github.com>
2021-11-30 08:31:51 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Atividade da Labenu
-const PrimeiroNome = 'Andreyna';
-const Sobrenome = 'Carvalho';
-const Idade = 20;
-const Estudante = 'Sim'; 
-
-let total = PrimeiroNome + ' ' + Sobrenome + ', ' + Idade + ', ' + Estudante + '.';
-
-console.log(total)
