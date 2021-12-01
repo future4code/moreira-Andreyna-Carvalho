97623d4e3fe6c5851a0a6d2820ba971592a47230
andreyna1808 <andreyna.m.carvalho@gmail.com>
2021-12-01 16:22:51 -0300

    Atividade de String

---
+++ modulo1/strings-arrays/script.js
@@ -0,0 +1,38 @@
+// ----- Atividade 1 -------
+let Usuario = prompt('Qual o seu nome?')
+let Email = prompt('Qual o seu email?')
+
+let resultado = "O e-mail " + Email + " foi cadastrado com sucesso. Seja bem-vinda(o), " + Usuario + "!";
+
+console.log(resultado)
+
+// ----- Atividade 2 -------
+let Food = ['Japonesa', 'Pizza', 'Dogão de rua', 'Baião de dois', 'Tutu de feijão com ovo']
+console.log(Food)
+console.log('Essas são minhas comidas preferidas: ')
+console.log(Food[0])
+console.log(Food[1])
+console.log(Food[2])
+console.log(Food[3])
+console.log(Food[4])
+// Creio que tinha uma forma mais facil, porém não lembro, se tu viu isso e puder ajudar.
+
+let Question = prompt('Qual sua comida preferida?')
+let i = 1;
+Food[i] = Question;
+console.log(Food)
+
+// ----- Atividade 3 -------
+let ListaDeTarefas = [];
+let Tarefa1 = prompt('Qual sua primeira tarefa do dia?')
+let Tarefa2 = prompt('Qual a sua segunda tarefa do dia?')
+let Tarefa3 = prompt('Qual a sua terceira tarefa do dia?')
+
+ListaDeTarefas.push(Tarefa1,Tarefa2,Tarefa3);
+console.log(ListaDeTarefas)
+
+
+
+console.log(ListaDeTarefas.splice(2))
+// voce deve percorrer seu array com lenght
+// e substituir pelo numero proposto
