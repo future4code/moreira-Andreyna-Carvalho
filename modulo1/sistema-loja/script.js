128e81896341c6ac9c0bd51383a4d32225c2af40
andreyna1808 <andreyna.m.carvalho@gmail.com>
2021-12-14 18:21:24 -0300

    Atividade de sistema-loja

---
+++ modulo1/sistema-loja/script.js
@@ -0,0 +1,12 @@
+// Exercício de fixação JS
+function carrosVendidos(qtdeCarrosVendidos, valorTotalVendas) {
+    let precoPorCarro = valorTotalVendas/qtdeCarrosVendidos
+
+    if (qtdeCarrosVendidos == 0){
+        return 2000
+    }
+    
+    let resultado = 2000 + (100 * qtdeCarrosVendidos + (qtdeCarrosVendidos * (precoPorCarro * 5/100)))
+    return resultado
+}
+console.log(carrosVendidos(0, 2000))
