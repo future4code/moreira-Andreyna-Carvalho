00e85c45d0e22e1874acd22528f3d174daba7fc0
andreyna1808 <andreyna.m.carvalho@gmail.com>
2021-12-15 16:20:35 -0300

    Estilização do sistema-loja

--- modulo1/sistema-loja/script.js
+++ modulo1/sistema-loja/script.js
@@ -1,4 +1,4 @@
-// Exercício de fixação JS
+// ----------- Exercício de fixação JS - 01 ----------
 function carrosVendidos(qtdeCarrosVendidos, valorTotalVendas) {
     let precoPorCarro = valorTotalVendas/qtdeCarrosVendidos
 
@@ -9,4 +9,17 @@ function carrosVendidos(qtdeCarrosVendidos, valorTotalVendas) {
     let resultado = 2000 + (100 * qtdeCarrosVendidos + (qtdeCarrosVendidos * (precoPorCarro * 5/100)))
     return resultado
 }
-console.log(carrosVendidos(0, 2000))
+console.log(carrosVendidos(0, 2000)) // exemplo de teste
+
+// ----------- Exercício de fixação JS - 02 ----------
+
+// As maças custam 1.3 se for comprado menos de 12 <
+// Custaram 1 real se compradas ao menos 12 >=
+function calculaPrecoTotal(quantidade){
+    if (quantidade < 12){
+        return quantidade * 1.3
+    }
+    else{
+        return quantidade * 1
+    }
+}
