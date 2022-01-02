283e0ab78a6de167fe63e61722855ef1b7253a48
andreyna1808 <andreyna.m.carvalho@gmail.com>
2022-01-02 09:47:12 -0400

    Arrumei os botoes dos projetos

--- modulo1/projeto-portfolio/script.js
+++ modulo1/projeto-portfolio/script.js
@@ -1,51 +1,44 @@
 let um = document.querySelector('.um')
+let fantasmaUm = document.querySelector('.fantasmaUm')
 function mouseOver(){
-    um.innerHTML = ` <h2> Pokedex</h2>
-    <p> Foi elaborada uma pokedex com o consumo de uma API, utilizando JavaScript, bruto para treinar minha lógica e testar os conhecimentos que adquiri no tempo ocioso</p>
-    <button> <a href="https://github.com/andreyna1808/ProjetoFixacao/tree/main/pokedex" target="_blank"> GitHub</a></button>
-    <button> <a href="https://andreyna1808.github.io/ProjetoFixacao/pokedex/index.html" target="_blank"> Rodando</a></button>
-    `
     um.style.backgroundColor = 'rgb(48, 46, 46)';
-}
-function mouseOut() {
-    um.innerHTML += "<h3> Você já viu?! </h3>"
+    fantasmaUm.style.opacity = 1;
 }
 
 let dois = document.querySelector('.dois')
+let fantasmaDois = document.querySelector('.fantasmaDois')
 function mouseOver1(){
-    dois.innerHTML = ` <h2> Pedra Papel Tesoura</h2>
-    <p> Foi criado um jogo de Pedra Papel Tesoura com js bruto</p>
-    <button> <a href="https://github.com/andreyna1808/ProjetoFixacao/tree/main/Pedra-Papel-Tesoura" target="_blank"> GitHub</a></button>
-    <button> <a href="https://andreyna1808.github.io/ProjetoFixacao/Pedra-Papel-Tesoura/index.html" target="_blank"> Rodando</a></button>
-    `
     dois.style.backgroundColor = 'rgb(48, 46, 46)';
-}
-function mouseOut1() {
-    dois.innerHTML += "<h3> Você já viu?! </h3>"
+    fantasmaDois.style.opacity = 1;
 }
 
 let tres = document.querySelector('.tres')
+let fantasmaTres = document.querySelector('.fantasmaTres')
 function mouseOver2(){
-    tres.innerHTML = ` <h2> Lista de Desejos</h2>
-    <p> Foi criado uma lista no qual pode colocar o nome, preço e idade, pretendo otimizar ela em breve</p>
-    <button> <a href="https://github.com/andreyna1808/ProjetoFixacao/tree/main/Lista" target="_blank"> GitHub</a></button>
-    <button> <a href="https://andreyna1808.github.io/ProjetoFixacao/Lista/index.html" target="_blank"> Rodando</a></button>
-    `
     tres.style.backgroundColor = 'rgb(48, 46, 46)';
-}
-function mouseOut2() {
-    tres.innerHTML += "<h3> Você já viu?! </h3>"
+    fantasmaTres.style.opacity = 1;
 }
 
 let quatro = document.querySelector('.quatro')
+let fantasmaQuatro = document.querySelector('.fantasmaQuatro')
 function mouseOver3(){
-    quatro.innerHTML = ` <h2> Você realmente é OTAKU?</h2>
-    <p> Uma brincadeira de perguntas sobre rap de animes, se acerta mais de 80%, eitahh você é um OTAKU!! já bora amigar</p>
-    <button> <a href="https://github.com/andreyna1808/Quiz" target="_blank"> GitHub</a></button>
-    <button> <a href="https://andreyna1808.github.io/Quiz/" target="_blank"> Rodando</a></button>
-    `
     quatro.style.backgroundColor = 'rgb(48, 46, 46)';
+    fantasmaQuatro.style.opacity = 1;
+}
+
+function mouseOut() {
+    fantasmaUm.style.opacity = 0;
+    um.style.backgroundColor = "rgb(100, 7, 25)";
+}
+function mouseOut1() {
+    fantasmaDois.style.opacity = 0;
+    dois.style.backgroundColor = "rgb(11, 61, 61)";
+}
+function mouseOut2() {
+    fantasmaTres.style.opacity = 0;
+    tres.style.backgroundColor = "darkblue";
 }
 function mouseOut3() {
-    quatro.innerHTML += "<h3> Você já viu?! </h3>"
+    fantasmaQuatro.style.opacity = 0;
+    quatro.style.backgroundColor = "rgb(145, 93, 30)";
 }
