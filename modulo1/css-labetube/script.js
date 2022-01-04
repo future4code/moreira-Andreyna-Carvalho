bb6060e0cb56f1684ea59a56586079eaabc8528e
andreyna1808 <andreyna.m.carvalho@gmail.com>
2022-01-04 11:20:42 -0400

    Atividade de css-labetube

---
+++ modulo1/css-labetube/script.js
@@ -0,0 +1,18 @@
+// Exercício de fixação!
+function criarArrayNomesAnimais() {
+    const animais = [
+      { nome: "Cachorro", classificacao: "mamífero" },
+      { nome: "Papagaio", classificacao: "ave" },
+      { nome: "Gato", classificacao: "mamífero" },
+      { nome: "Carpa", classificacao: "peixe" },
+      { nome: "Pomba", classificacao: "ave" }
+    ]
+
+    const nomeAnimal = (item) => {
+        return item.nome
+    }
+    let nomes = animais.map(nomeAnimal)
+    return nomes
+
+}
+console.log(criarArrayNomesAnimais());
