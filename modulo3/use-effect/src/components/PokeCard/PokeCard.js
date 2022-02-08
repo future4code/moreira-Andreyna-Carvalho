d57f1fb8c8d09716ffbf13c86419cfd5d6d7d796
andreyna1808 <andreyna.m.carvalho@gmail.com>
2022-02-08 16:03:24 -0400

    Transformei em componente funcional e usando o useEffect

---
+++ modulo3/use-effect/src/components/PokeCard/PokeCard.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react/cjs/react.development";
+
+function PokeCard(){
+  const [pokemon, setPokemon] = useState({});
+
+  const pegaPokemon = (pokeName) => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+      .then(response => {
+        setPokemon(response.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+
+    pegaPokemon(pokemon)
+  }, [pokemon])
+  
+    return (
+      <div>
+        <p>{pokemon.name}</p>
+        <p>{pokemon.weight} Kg</p>
+        {pokemon.types && <p>{pokemon.types[0].type.name}</p>}
+        {pokemon.sprites && (
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+        )}
+      </div>
+    );
+}
+
+export default PokeCard;
