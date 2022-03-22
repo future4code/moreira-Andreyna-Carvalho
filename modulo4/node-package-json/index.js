0f44f611f4f13b7c201fecc3649f335571f5a607
andreyna1808 <andreyna.m.carvalho@gmail.com>
2022-03-22 15:16:03 -0400

    iniciando o projeto

---
+++ modulo4/node-package-json/index.js
@@ -0,0 +1,13 @@
+// process.argv
+
+const information = {
+  nome: 'Andreyna',
+  idade: 20
+}
+
+const age = Number(process.agv[2])
+const name = process.agv[2]
+
+const info = `"Olá, ${age}! Você tem ${name} anos."`
+
+console.log(information[age]);
