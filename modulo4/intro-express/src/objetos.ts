4da0f99f973bd8bb08e113a9665bbf8a605460dd
andreyna1808 <andreyna.m.carvalho@gmail.com>
2022-03-29 16:12:25 -0400

    atualizando atividades

---
+++ modulo4/intro-express/src/objetos.ts
@@ -0,0 +1,18 @@
+export const dados = [
+  {
+    id: 0,
+    nome: 'Andreyna',
+    idade: 20,
+    phone: 48,
+    email: 'andreyna.m.carvalho@gmail.com',
+    website: 'https://www.linkedin.com/in/andreyna-carvalho-997273231/'
+  },
+  {
+    id: 1,
+    nome: 'Willhan',
+    idade: 22,
+    phone: 66,
+    email: 'willhanmarques09@gmail.com',
+    website: 'https://www.linkedin.com/in/will-marques-a19879228/'
+  }
+]
